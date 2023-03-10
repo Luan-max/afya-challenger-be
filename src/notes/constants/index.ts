9ef6c0391d484b7c5b11597ce0b0932bfaf8fabb
Luan-max <luannlopessiva@outlook.com>
2023-03-10 20:06:40 -0300

    feat: add notes entity

---
+++ src/notes/constants/index.ts
@@ -0,0 +1,12 @@
+export const NOTES_DTO_PATIENT_NAME_DESCRIPTION =
+    'The name of the patient related to the medical consultation note',
+  NOTES_DTO_DATE_DESCRIPTION = 'The date of the medical consultation note',
+  NOTE_DTO_NOTES_DESCRIPTION = 'The notes of the medical consultation',
+  NOTES_DTO_APPOINTMENT_ID_DESCRIPTION =
+    'The ID of the appointment related to the medical consultation note';
+
+export const NOTES_DTO_APPOINTMENT_ID_EXAMPLE = '1',
+  NOTES_DTO_PATIENT_NAME_EXAMPLE = 'John Doe',
+  NOTES_DTO_DATE_EXAMPLE = '2022-02-28T10:00:00Z',
+  NOTES_DTO_NOTES_EXAMPLE =
+    'Patient presents with symptoms of a cold. Prescribed medication and advised rest.';
---
+++ src/notes/entities/note.entity.ts
@@ -0,0 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Appointment } from '../../appointments/entities/appointment.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import {
+  NOTES_DTO_APPOINTMENT_ID_DESCRIPTION,
+  NOTES_DTO_APPOINTMENT_ID_EXAMPLE,
+  NOTES_DTO_PATIENT_NAME_DESCRIPTION,
+  NOTES_DTO_PATIENT_NAME_EXAMPLE,
+  NOTES_DTO_NOTES_EXAMPLE,
+  NOTE_DTO_NOTES_DESCRIPTION,
+  NOTES_DTO_DATE_DESCRIPTION,
+  NOTES_DTO_DATE_EXAMPLE,
+} from '../constants';
+
+@Entity()
+export class Note {
+  @PrimaryGeneratedColumn()
+  @ApiProperty({
+    description: NOTES_DTO_APPOINTMENT_ID_DESCRIPTION,
+    example: NOTES_DTO_APPOINTMENT_ID_EXAMPLE,
+  })
+  id: number;
+
+  @Column()
+  @ApiProperty({
+    description: NOTES_DTO_PATIENT_NAME_DESCRIPTION,
+    example: NOTES_DTO_PATIENT_NAME_EXAMPLE,
+  })
+  patientName: string;
+
+  @Column()
+  @ApiProperty({
+    description: NOTES_DTO_DATE_DESCRIPTION,
+    example: NOTES_DTO_DATE_EXAMPLE,
+  })
+  date: Date;
+
+  @Column()
+  @ApiProperty({
+    description: NOTES_DTO_NOTES_EXAMPLE,
+    example: NOTE_DTO_NOTES_DESCRIPTION,
+  })
+  note: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  @ManyToOne(() => Appointment)
+  @ApiProperty({
+    description: NOTES_DTO_APPOINTMENT_ID_DESCRIPTION,
+    example: NOTES_DTO_APPOINTMENT_ID_EXAMPLE,
+  })
+  appointmentId: string;
+}
